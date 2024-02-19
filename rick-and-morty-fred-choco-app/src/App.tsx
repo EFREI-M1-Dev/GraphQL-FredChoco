4edf5a4ea803f77dc67203e916874f26cae72f09
Chocolatiines <pimooshwoom@gmail.com>
2024-02-19 10:57:18 +0100

    add base query

--- rick-and-morty-fred-choco-app/src/App.tsx
+++ rick-and-morty-fred-choco-app/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useQuery} from 'urql'
+
+const CHARACTER_QUERY = `
+    query {
+      characters {
+        info {
+          count
+        }
+        results {
+          name
+        }
+      }
+    }`
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [result] = useQuery({query: CHARACTER_QUERY})
+    const {data, fetching, error} = result
+    console.log(data)
+    if (fetching) return <div>Loading...</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    if (error) return <pre>{error.message}</pre>
+
+    return (
+        <>
+            <h1>Character</h1>
+            <ul>
+                {data.characters.results.map((character: any) => (
+                    <li key={character.name}>{character.name}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App
